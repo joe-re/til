d6f37a3f3197e5a3e8c52e247bf0a348cbafe22d
joe-re <joe.tialtngo@gmail.com>
2016-03-23 01:28:53 +0900

    learn rust: enumerate

--- rust/loop/src/main.rs
+++ rust/loop/src/main.rs
@@ -1,10 +1,17 @@
 fn main() {
     println!("Hello, world!");
     for_test();
+    enumerate_test();
 }
 
 fn for_test() {
     for x in 0..10 {
         println!("{:?}", x);
+    }
+}
+
+fn enumerate_test() {
+    for (i, j) in (5..10).enumerate() {
+        println!("i = {}, j = {}", i, j);
     }
 }
