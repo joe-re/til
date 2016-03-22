f7e255892414ba9d264111c7d486f2b00d3d7d4d
joe-re <joe.tialtngo@gmail.com>
2016-03-23 01:35:58 +0900

    learn rust: Ending iteration early

--- rust/loop/src/main.rs
+++ rust/loop/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     println!("Hello, world!");
     for_test();
     enumerate_test();
+    break_test();
+    continue_test();
 }
 
 fn for_test() {
@@ -13,5 +15,21 @@ fn for_test() {
 fn enumerate_test() {
     for (i, j) in (5..10).enumerate() {
         println!("i = {}, j = {}", i, j);
+    }
+}
+
+fn break_test() {
+    let mut x = 5;
+    loop {
+        x += x - 3;
+        println!("{}", x);
+        if x % 5 == 0 { break; }
+    }
+}
+
+fn continue_test() {
+    for x in 0..10 {
+        if x % 2 == 0 { continue; }
+        println!("{}", x);
     }
 }
