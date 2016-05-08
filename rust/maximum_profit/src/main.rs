e04c895f0078c2a6eeb5cd2e8e80f7f007cca3c1
joe-re <joe.tialtngo@gmail.com>
2016-05-09 03:02:15 +0900

    ALDS 1_1 Maximum Profit by rust

--- rust/maximum_profit/src/main.rs
+++ rust/maximum_profit/src/main.rs
@@ -1,12 +1,20 @@
 use std::io;
+use std::cmp;
+
 fn main() {
-    const MAX:i32 = 20000;
     let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(_n) => {
-          println!("{}", input);
-        }
-        Err(error) => println!("error: {}", error),
+    io::stdin().read_line(&mut input).unwrap();
+    let n = input.replace("\n", "").parse::<i32>().unwrap();
+    let a: Vec<i32> = (0..n).map(|_x| {
+        input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        return input.replace("\n", "").parse::<i32>().unwrap();
+    }).collect();
+    let mut maxv = -20000;
+    let mut minv = a[0];
+    for i in 1..n {
+        maxv = cmp::max(maxv, a[i as usize] - minv);
+        minv = cmp::min(minv, a[i as usize]);
     }
-    println!("Hello, world!");
+    println!("{:?}", maxv);
 }
