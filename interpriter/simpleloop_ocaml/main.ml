970fceec6924001ca9eb779011fa0cc1a9ea1d7f
joe-re <joe.tialtngo@gmail.com>
2017-01-10 03:13:50 +0900

    ocaml

--- interpriter/simpleloop_ocaml/main.ml
+++ interpriter/simpleloop_ocaml/main.ml
@@ -1,11 +1,30 @@
 open Util;;
 
+let stck = ref [] in
 let quit_loop = ref false in
 while not !quit_loop do
   print_string "->";
-  let str = read_line () in
-  if compare_ign_case(str) then
+  let s = read_line () in
+  if compare_ign_case s "quit" then
     quit_loop := true
+  else if compare_ign_case s "printstack" then
+    let tmp = print_stack !stck in
+    print_endline "--- end stack ---"
   else
-    print_endline(str)
+    print_endline s;
+    match s with
+    "+" ->
+      stck := add_top_element !stck;
+      show_first_element !stck
+    | "-" ->
+      stck := sub_top_element !stck;
+      show_first_element !stck
+    | "*" ->
+      stck := ply_top_element !stck;
+      show_first_element !stck
+    | "/" ->
+      stck := div_top_element !stck;
+      show_first_element !stck
+    | other ->
+      stck :=  int_of_string s::!stck
 done
--- interpriter/simpleloop_ocaml/util.ml
+++ interpriter/simpleloop_ocaml/util.ml
@@ -1,1 +1,36 @@
-let compare_ign_case str = String.lowercase_ascii(str) = "quit"
+let compare_ign_case str ign = String.lowercase_ascii(str) = ign
+
+type elem_t = {
+  a: int;
+  b: int;
+}
+
+let calc_top_element f lst = match lst with
+  [] -> print_endline "empty list"; lst
+  | first::second::rest -> f first second::rest
+  | first::rest -> print_endline "lack of operand"; lst
+
+let add_top_element lst =
+  let f first rest_result = first + rest_result in
+  calc_top_element f lst
+
+let sub_top_element lst =
+  let f first rest_result = first - rest_result in
+  calc_top_element f lst
+
+let ply_top_element lst =
+  let f first rest_result = first * rest_result in
+  calc_top_element f lst
+
+let div_top_element lst =
+  let f first rest_result = first / rest_result in
+  calc_top_element f lst
+
+let show_first_element lst = match lst with
+  [] -> print_endline "empty list"
+  | first::rest -> print_endline (string_of_int first)
+
+let rec print_stack lst = match lst with
+  [] -> None
+  | first::rest -> print_endline (string_of_int first); print_stack rest
+
