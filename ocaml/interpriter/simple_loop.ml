e490c0bf140ea7e9389f77523390dcc7b2040666
joe-re <joe.tialtngo@gmail.com>
2017-04-03 04:43:40 +0900

    implements add top stack

--- ocaml/interpriter/simple_loop.ml
+++ ocaml/interpriter/simple_loop.ml
@@ -6,6 +6,19 @@ let print_stack lst =
     [] -> ()
     | first::rest -> print_endline(string_of_float first); loop rest in loop lst
 
+let get_top_2_elem lst = match lst with
+  [] -> None
+  | first::second::rest -> Some(first, second, rest)
+  | first::rest -> None
+
+let add_top_stack lst =
+  let elems = get_top_2_elem lst in
+  match elems with
+  | Some(first, second, rest) ->
+    print_endline(string_of_float(first +. second));
+    (first +. second)::rest
+  | None -> print_endline "lack of operand"; lst
+
 let () =
   let stck = ref [] in
   let quit_loop = ref false in
@@ -17,6 +30,11 @@ let () =
     else if comp_ign_case "printstack" str then
       print_stack !stck
     else
-      stck := float_of_string str::!stck
+      stck :=  match str.[0] with
+        | '+' -> add_top_stack !stck
+        | '-' -> !stck
+        | '/' -> !stck
+        | '*' -> !stck
+        | _ -> float_of_string str::!stck
   done
 
