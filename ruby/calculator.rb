62673543770e23a0d4ca4953093f8b87e047b349
joe-re <joe.tialtngo@gmail.com>
2017-08-14 00:13:19 +0900

    variable

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -1,23 +1,27 @@
 require 'minruby'
 
-def evaluate(tree)
+def evaluate(tree, env = {})
   return tree[1] if tree[0] == 'lit'
   case tree[0]
-  when '+' then evaluate(tree[1]) + evaluate(tree[2])
-  when '-' then evaluate(tree[1]) - evaluate(tree[2])
-  when '*' then evaluate(tree[1]) * evaluate(tree[2])
-  when '/' then evaluate(tree[1]) / evaluate(tree[2])
-  when '**' then evaluate(tree[1]) ** evaluate(tree[2])
-  when '%' then evaluate(tree[1]) % evaluate(tree[2])
-  when '>' then evaluate(tree[1]) > evaluate(tree[2])
-  when '<' then evaluate(tree[1]) < evaluate(tree[2])
-  when '==' then evaluate(tree[1]) == evaluate(tree[2])
-  when 'func_call' then p evaluate(tree[2])
+  when '+' then evaluate(tree[1], env) + evaluate(tree[2], env)
+  when '-' then evaluate(tree[1], env) - evaluate(tree[2], env)
+  when '*' then evaluate(tree[1], env) * evaluate(tree[2], env)
+  when '/' then evaluate(tree[1], env) / evaluate(tree[2], env)
+  when '%' then evaluate(tree[1], env) % evaluate(tree[2], env)
+  when '>' then evaluate(tree[1], env) > evaluate(tree[2], env)
+  when '<' then evaluate(tree[1], env) < evaluate(tree[2], env)
+  when '==' then evaluate(tree[1], env) == evaluate(tree[2], env)
+  when '**' then evaluate(tree[1], env) ** evaluate(tree[2], env)
+  when 'func_call' then p evaluate(tree[2], env)
+  when 'var_assign'
+    env[tree[1]] = evaluate(tree[2], env)
+  when 'var_ref'
+    env[tree[1]]
   when 'stmts'
     i = 1
     last = nil
     while tree[i] != nil
-      last = evaluate(tree[i])
+      last = evaluate(tree[i], env)
       i += 1
     end
     last
