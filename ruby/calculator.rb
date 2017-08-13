3bb9de3fda01601bf2fad210c31bc0c1f92a7c4b
joe-re <joe.tialtngo@gmail.com>
2017-08-13 23:48:15 +0900

    func_call

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -2,18 +2,17 @@ require 'minruby'
 
 def evaluate(tree)
   return tree[1] if tree[0] == 'lit'
-  left = evaluate tree[1]
-  right = evaluate tree[2]
   case tree[0]
-  when '+' then left + right
-  when '-' then left - right
-  when '*' then left * right
-  when '/' then left / right
-  when '**' then left**right
-  when '%' then left % right
-  when '>' then left > right
-  when '<' then left < right
-  when '==' then left == right
+  when '+' then evaluate(tree[1]) + evaluate(tree[2])
+  when '-' then evaluate(tree[1]) - evaluate(tree[2])
+  when '*' then evaluate(tree[1]) * evaluate(tree[2])
+  when '/' then evaluate(tree[1]) / evaluate(tree[2])
+  when '**' then evaluate(tree[1]) ** evaluate(tree[2])
+  when '%' then evaluate(tree[1]) % evaluate(tree[2])
+  when '>' then evaluate(tree[1]) > evaluate(tree[2])
+  when '<' then evaluate(tree[1]) < evaluate(tree[2])
+  when '==' then evaluate(tree[1]) == evaluate(tree[2])
+  when 'func_call' then p evaluate(tree[2])
   end
 end
 
@@ -25,9 +24,13 @@ def max(tree, max_num = 0)
   max(tree[2], left)
 end
 
-p evaluate(minruby_parse('2%4'))
-p evaluate(minruby_parse('1 + 1 == 2'))
-p evaluate(minruby_parse('1 + 1 < 2'))
-p evaluate(minruby_parse('1 + 1 < 3'))
-p max(minruby_parse('1 + 2 * 3'))
-p max(minruby_parse('1 + 4 + 3'))
+# p evaluate(minruby_parse('2%4'))
+# p evaluate(minruby_parse('1 + 1 == 2'))
+# p evaluate(minruby_parse('1 + 1 < 2'))
+# p evaluate(minruby_parse('1 + 1 < 3'))
+# p max(minruby_parse('1 + 2 * 3'))
+# p max(minruby_parse('1 + 4 + 3'))
+
+str = minruby_load()
+tree = minruby_parse(str)
+evaluate(tree)
